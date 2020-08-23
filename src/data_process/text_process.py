6b56ef1313a2684c50129309d629f9fa84495a08
atgm1113 <atgm1113@gmail.com>
2020-08-23 14:29:30 +0300

    Adds bert tokenizer from huggingface transformers lib, Adds bert dataset generation

--- src/data_process/text_process.py
+++ src/data_process/text_process.py
@@ -1,20 +1,36 @@
-import bert
+import transformers
+from transformers import BertTokenizerFast
 
 
-def text_to_bert_ids(bert_tokenizer : bert.bert_tokenization.FullTokenizer, text : str) -> list:
-    if not isinstance(bert_tokenizer, bert.bert_tokenization.FullTokenizer):
-        raise TypeError("text_to_bert_ids: expected bert_tokenizer of type bert.bert_tokenization.FullTokenizer, got {0}".format(type(bert_tokenizer)))
+def text_to_bert_ids(bert_tokenizer : BertTokenizerFast, text : str) -> list:
+    """
+    Encoding text into bert input.
+
+    Parameters:
+        bert_tokenizer (BertTokenizerFast) : bert fast tokenizer form huggingface transformers.
+        text (str) : text that needs to be encoded.
+    Returns:
+        input_ids (list) : tokens ids.
+        token_type_ids (list) : types of tokens ids.
+        attention_mask (list) : attention mask.
+    """
+    if not isinstance(bert_tokenizer, BertTokenizerFast):
+        raise TypeError("text_to_bert_ids: expected bert_tokenizer of type BertTokenizerFast, got {0}".format(type(bert_tokenizer)))
 
     if not isinstance(text, str):
         raise TypeError("text_to_bert_ids: expected text of type str, got {0}".format(type(text)))
 
-    ids = bert_tokenizer.tokenize(text)
-    ids = bert_tokenizer.convert_tokens_to_ids(ids)
-    return ids
+    encoding_dict = bert_tokenizer.encode_plus(text, padding="max_length", max_length=128)
 
-def bert_ids_to_text(bert_tokenizer : bert.bert_tokenization.FullTokenizer, ids : list) -> str:
-    if not isinstance(bert_tokenizer, bert.bert_tokenization.FullTokenizer):
-        raise TypeError("bert_ids_to_text: expected bert_tokenizer of type bert.bert_tokenization.FullTokenizer, got {0}".format(type(bert_tokenizer)))
+    input_ids = encoding_dict["input_ids"]
+    token_type_ids = encoding_dict["token_type_ids"]
+    attention_mask = encoding_dict["attention_mask"]
+
+    return input_ids, token_type_ids, attention_mask
+
+def bert_ids_to_text(bert_tokenizer : BertTokenizerFast, ids : list) -> str:
+    if not isinstance(bert_tokenizer, BertTokenizerFast):
+        raise TypeError("bert_ids_to_text: expected bert_tokenizer of type BertTokenizerFast, got {0}".format(type(bert_tokenizer)))
 
     tokens = bert_tokenizer.convert_ids_to_tokens(ids)
     text = " ".join(tokens)
--- src/main/build_bert_dataset.py
+++ src/main/build_bert_dataset.py
@@ -6,6 +6,8 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_hub as tf_hub
 import bert
+import transformers
+from transformers import BertTokenizerFast
 from src.data_process.dataset_builder import numpy_datasets_from_csv
 from src.data_process.text_process import text_to_bert_ids, bert_ids_to_text
 from src.model.bert_semantic_classifier import get_bert
@@ -18,14 +20,40 @@ TRAIN_CSV_PATH = pjoin(RAW_DATA_FOLDER_PATH, "train.csv")
 TEST_CSV_PATH = pjoin(RAW_DATA_FOLDER_PATH, "test.csv")
 # Bert data
 BERT_LAYER_HUB_URL = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2"
-BERT_SAVED_DATA_FOLDER_PATH = pjoin(cwd, "data/bert_data")
+BERT_DATASETS_FOLDER_PATH = pjoin(cwd, "data/processed/bert_datasets")
 
 def main():
     X, y = numpy_datasets_from_csv(TRAIN_CSV_PATH)
-    bert_layer, bert_tokenizer = get_bert(BERT_LAYER_HUB_URL)
+    bert_layer, vocab = get_bert(BERT_LAYER_HUB_URL)
+    bert_tkn = BertTokenizerFast(vocab)
     X_list = X.tolist()
 
-    
+    X_ids = []
+    X_ids_types = []
+    X_attn_mask = []
+
+    print(f"Got {len(X)} texts, processing...")
+    for txt in X_list:
+        ids, types, attention = text_to_bert_ids(bert_tkn, txt)
+        X_ids.append(ids)
+        X_ids_types.append(types)
+        X_attn_mask.append(attention)
+
+    X_ids = np.array(X_ids, dtype=int)
+    X_ids_types = np.array(X_ids_types, dtype=int)
+    X_attn_mask = np.array(X_attn_mask, dtype=int)
+
+    print("\nGenerated bert datasets:")
+    print(f"X_ids shape is {X_ids.shape}")
+    print(f"X_ids_types shape is {X_ids_types.shape}")
+    print(f"X_attn_mask shape is {X_attn_mask.shape}")
+    print(f"y shape is {y.shape}")
+
+    np.save(pjoin(BERT_DATASETS_FOLDER_PATH, "X_ids.npy"), X_ids)
+    np.save(pjoin(BERT_DATASETS_FOLDER_PATH, "X_ids_types.npy"), X_ids_types)
+    np.save(pjoin(BERT_DATASETS_FOLDER_PATH, "X_attn_mask.npy"), X_attn_mask)
+    np.save(pjoin(BERT_DATASETS_FOLDER_PATH, "y.npy"), y)
+    print(f"\nSaved datasets to {BERT_DATASETS_FOLDER_PATH}")
 
 if __name__ == "__main__":
     main()
